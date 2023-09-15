4e592c5a8ba0755b55296424311c4fd0719345a5
tentegess <tenagent2222@gmail.com>
2023-09-15 09:59:30 +0200

    admin cards design improve

---
+++ services/admin_service.py
@@ -0,0 +1,14 @@
+from utils.db_config import db
+from models.Users import Users
+from models.user_tokens import Tokens
+from flask import flash, redirect, session, render_template, request
+from utils import LANG
+
+
+def get_site_stats():
+    stats = {
+        "user_count": Users.query.count(),
+        "admins": Users.query.filter_by(is_admin=True).count(),
+        "banned": Users.query.filter_by(suspended=True).count(),
+    }
+    return stats
--- views/admin_views.py
+++ views/admin_views.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template, redirect, request
-
+from services import admin_service
 
 admin = Blueprint("admin", __name__, static_folder="static", template_folder="templates")
 
 
-
 @admin.get("/")
 def admin_main():
-    return render_template("admin/adm_index.html")
+    stats = admin_service.get_site_stats()
+    return render_template("admin/adm_index.html", stats=stats)
