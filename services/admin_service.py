04d1c38572c6812559faaf2e2300c5bac62aed6f
tentegess <tenagent2222@gmail.com>
2023-09-20 13:16:00 +0200

    minor changes

--- services/admin_service.py
+++ services/admin_service.py
@@ -4,6 +4,7 @@ from models.user_tokens import Tokens
 from flask import flash, redirect, session, render_template, request
 from utils import LANG
 
+
 def get_site_stats():
     stats = {
         "user_count": Users.query.count(),
@@ -12,9 +13,10 @@ def get_site_stats():
     }
     return stats
 
+
 def get_users(query_model):
 
     query = f'%{query_model.query}%'
     result = (Users.query.with_entities(Users.id, Users.name, Users.email, Users.suspended, Users.is_admin)
-              .filter(Users.name.like(query) | Users.email.like(query)).all())
+              .filter(Users.name.ilike(query) | Users.email.ilike(query)).all())
     return result
--- view_models/admin/search_user_view_model.py
+++ view_models/admin/search_user_view_model.py
@@ -7,3 +7,4 @@ class SearchUserViewModel(BaseViewModel):
     def __init__(self):
         super().__init__()
         self.query = str(self.req_dict.query)
+        print(self.req_dict)
